1a6ae0b458b910c5b621a951d2c974979917cf03
rebeccasaxon <rebeccaannsaxon@gmail.com>
2023-04-12 17:56:05 -0500

    Add Query

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,30 +1,5 @@
 
-
-
-  street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  })
-
-  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
-
- 
-  let myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [street]
-  });
-
-
-let queryURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-
-
-
-
-d3.json(queryURL).then(function (data) {
+d3.json(https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson).then(function (data) {
   
   createFeatures(data.features);
   console.log(data.features)
