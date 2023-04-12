8f6b37b41e2c171443fd0eb785703e3df153855c
rebeccasaxon <rebeccaannsaxon@gmail.com>
2023-04-12 17:57:04 -0500

    Add layers

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,6 +5,23 @@ d3.json(https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojs
   console.log(data.features)
 });
 
+  // Base
+  street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  })
+
+  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  });
+
+  let myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [street]
+  });
+
 function createFeatures(earthquakeData) {
 
   
